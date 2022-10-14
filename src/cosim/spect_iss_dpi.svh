6ce0f531978ef0ebaa289ed7c00fbf4287758b4b
Marek Santa <marek.santa@tropicsquare.com>
2022-10-14 16:39:11 +0200

    change data type from int to int unsigned

--- src/cosim/spect_iss_dpi.svh
+++ src/cosim/spect_iss_dpi.svh
@@ -18,7 +18,7 @@
  *  @returns 0 - Initialization succefull,
  *           1 - Initialization failed.
  */
-import "DPI-C" function int spect_dpi_init();
+import "DPI-C" function int unsigned spect_dpi_init();
 
 /**
  *  @brief Finish using simulation model (performs memory clean-up)
@@ -44,7 +44,7 @@ import "DPI-C" function void spect_dpi_reset();
  * @returns Value read from the SPECT memory space.
  * @note Allows to read any existing memory from SPECT memory space.
  */
-import "DPI-C" function int spect_dpi_get_memory(int addr);
+import "DPI-C" function int unsigned spect_dpi_get_memory(int unsigned addr);
 
 /**
  * @brief Stores word to SPECT memory space
@@ -52,7 +52,7 @@ import "DPI-C" function int spect_dpi_get_memory(int addr);
  * @param data Data to write
  * @note Allows to write any existing memory from SPECT memory space (even ROM).
  */
-import "DPI-C" function void spect_dpi_set_memory(int addr, int data);
+import "DPI-C" function void spect_dpi_set_memory(int unsigned addr, int unsigned data);
 
 /**
  * @brief Executes read from SPECT memory space via AHB.
@@ -61,7 +61,7 @@ import "DPI-C" function void spect_dpi_set_memory(int addr, int data);
  * @note Models access rights of SPECT on AHB (e.g. Data RAM IN is not readable).
  *       Read from invalid space returns all 0x0.
  */
-import "DPI-C" function int spect_dpi_ahb_read(int addr);
+import "DPI-C" function int unsigned spect_dpi_ahb_read(int unsigned addr);
 
 /**
  * @brief Executes write to SPECT memory space via AHB.
@@ -70,21 +70,21 @@ import "DPI-C" function int spect_dpi_ahb_read(int addr);
  * @note Models access rights of SPECT on AHB (e.g. Constant ROM is not writable)
  *       Write to invalid space is ignored.
  */
-import "DPI-C" function void spect_dpi_ahb_write(int addr, int data);
+import "DPI-C" function void spect_dpi_ahb_write(int unsigned addr, int unsigned data);
 
 /**
  * @brief Return Base address of a Memory within SPECT memory space
  * @param mem_type Type of memory to return base address
  * @returns Base address of memory within SPECT memory space.
  */
-import "DPI-C" function int spect_dpi_get_mem_base(dpi_mem_type_t mem_type);
+import "DPI-C" function int unsigned spect_dpi_get_mem_base(dpi_mem_type_t mem_type);
 
 /**
  * @brief Return size of a Memory within SPECT memory space
  * @param mem_type Type of memory to return base address
  * @returns Size of 'mem_type' memory of SPECT.
  */
-import "DPI-C" function int spect_dpi_get_mem_size(dpi_mem_type_t mem_type);
+import "DPI-C" function int unsigned spect_dpi_get_mem_size(dpi_mem_type_t mem_type);
 
 /**
  * @brief Returns value from part of General Purpose register.
@@ -96,7 +96,7 @@ import "DPI-C" function int spect_dpi_get_mem_size(dpi_mem_type_t mem_type);
  *                  8 - Bits 255:224
  * @returns Value in selected part of GPR register.
  */
-import "DPI-C" function int spect_dpi_get_gpr_part(int gpr, int part);
+import "DPI-C" function int unsigned spect_dpi_get_gpr_part(int unsigned gpr, int unsigned part);
 
 /**
  * @brief Stores value to part of General Purpose register.
@@ -108,7 +108,7 @@ import "DPI-C" function int spect_dpi_get_gpr_part(int gpr, int part);
  *                  8 - Bits 255:224
  * @param data - Data to store to GPR register part.
  */
-import "DPI-C" function void spect_dpi_set_gpr_part(int gpr, int part, int data);
+import "DPI-C" function void spect_dpi_set_gpr_part(int unsigned gpr, int unsigned part, int unsigned data);
 
 /**
  * @brief Get value of SPECT flag.
@@ -116,19 +116,19 @@ import "DPI-C" function void spect_dpi_set_gpr_part(int gpr, int part, int data)
  * @returns 0 - Flag is not set.
  *          1 - Flag is set.
  */
-import "DPI-C" function int spect_dpi_get_flag(dpi_flag_type_t flag_type);
+import "DPI-C" function int unsigned spect_dpi_get_flag(dpi_flag_type_t flag_type);
 
 /**
  * @brief Get SPECT program counter
  * @returns Value of program counter
  */
-import "DPI-C" function int spect_dpi_get_pc();
+import "DPI-C" function int unsigned spect_dpi_get_pc();
 
 /**
  * @brief Set SPECT program counter
  * @param value New value of program counter
  */
-import "DPI-C" function void spect_dpi_set_pc(int value);
+import "DPI-C" function void spect_dpi_set_pc(int unsigned value);
 
 /**
  * @brief Dump instruction.
@@ -136,7 +136,7 @@ import "DPI-C" function void spect_dpi_set_pc(int value);
  * @returns Instruction as it would be written in .S file.
  * @note return value is valid for whole duration of simulation.
  */
-import "DPI-C" function void spect_dpi_dump_instruction(int address, string buffer);
+import "DPI-C" function void spect_dpi_dump_instruction(int unsigned address, string buffer);
 
 /**
  * @brief Get part of SPECTs SRR register.
@@ -147,7 +147,7 @@ import "DPI-C" function void spect_dpi_dump_instruction(int address, string buff
  *                  8 - Bits 255:224
  * @returns Value in selected part of SRR register.
  */
-import "DPI-C" function int spect_dpi_get_srr(int part);
+import "DPI-C" function int unsigned spect_dpi_get_srr(int unsigned part);
 
 /**
  * @brief Get value in RAR stack.
@@ -158,19 +158,19 @@ import "DPI-C" function int spect_dpi_get_srr(int part);
  *                  N(X) - Number of X instructions executed since start of program.
  * @returns Value in RAR_STACK on 'address'.
  */
-import "DPI-C" function int spect_dpi_get_rar_value(int address);
+import "DPI-C" function int unsigned spect_dpi_get_rar_value(int unsigned address);
 
 /**
  * @brief Get RAR stack pointer.
  * @returns Current value of RAR stack pointer.
  */
-import "DPI-C" function int spect_dpi_get_rar_sp();
+import "DPI-C" function int unsigned spect_dpi_get_rar_sp();
 
 /**
  * @brief Push data for GRV instruction queried via RNG Handshake interface.
  * @param data Data to push to GRV queue.
  */
-import "DPI-C" function void spect_dpi_push_grv_queue(int data);
+import "DPI-C" function void spect_dpi_push_grv_queue(int unsigned data);
 
 /**
  * @brief Push data for GPK instruction queried via GPK Handshake interface.
@@ -178,15 +178,15 @@ import "DPI-C" function void spect_dpi_push_grv_queue(int data);
  * @param index Index of GPK queue (corresponds to value of spect_prk_type): 0 - 7.
  *              Corresponds to spect_prk_type[5:3] and immediate[2:0].
  */
-import "DPI-C" function void spect_dpi_push_gpk_queue(int data, int index);
+import "DPI-C" function void spect_dpi_push_gpk_queue(int unsigned data, int unsigned index);
 
 /**
  * @brief Get value of SPECT interrupt output
  * @param type Type of interrupt to obtain the value
- * @returns 0 - Interrupt not asserted.
- *          1 - Interrupt asserted.
+ * @returns 0 - interrupt not asserted.
+ *          1 - interrupt asserted.
  */
-import "DPI-C" function int spect_dpi_get_interrupt(dpi_int_type_t int_type);
+import "DPI-C" function int unsigned spect_dpi_get_interrupt(dpi_int_type_t int_type);
 
 /**
  *  @brief Assemble and load firmware, set Program counter to value of '_start' symbol.
@@ -195,7 +195,7 @@ import "DPI-C" function int spect_dpi_get_interrupt(dpi_int_type_t int_type);
  *           non-zero - Compilation failed.
  *  @note This function fails if the S file does not define '_start' symbol.
  */
-import "DPI-C" function int spect_dpi_assemble_and_load_firmware(string path);
+import "DPI-C" function int unsigned spect_dpi_assemble_and_load_firmware(string path);
 
 /**
  *  @brief Load HEX file to SPECT memory. This could be firmware or data RAM,
@@ -204,7 +204,7 @@ import "DPI-C" function int spect_dpi_assemble_and_load_firmware(string path);
  *  @returns 0 - Program assembled and loaded correctly
  *           non-zero - Loading of assembly failed.
  */
-import "DPI-C" function int spect_dpi_load_hex(string path);
+import "DPI-C" function int unsigned spect_dpi_load_hex(string path);
 
 /**
  *  @brief Execute single instruction of program, and remember how many clock cycles
@@ -218,14 +218,14 @@ import "DPI-C" function int spect_dpi_load_hex(string path);
  *           N - Number of instructions previous execution of instruction with the same
  *               took.
  */
-import "DPI-C" function int spect_dpi_program_step(int cycle_count);
+import "DPI-C" function int unsigned spect_dpi_program_step(int unsigned cycle_count);
 
 /**
  *  @brief Set Change reporting by model (pushing change events to SCHF - State Change FIFO)
  *  @param enable 0 - Disable change reporting
  *                1 - Enable change reporting
  */
-import "DPI-C" function void spect_dpi_set_change_reporting(int enable);
+import "DPI-C" function void spect_dpi_set_change_reporting(int unsigned enable);
 
 /**
  *  @brief Pop state change event from SCHF (State Change FIFO).
@@ -236,7 +236,7 @@ import "DPI-C" function void spect_dpi_set_change_reporting(int enable);
  *        every instruction. It is thus expected simulation side will anyway need to read
  *        program counter directly after each instruction!
  */
-//import "DPI-C" function int spect_dpi_get_model_change(dpi_state_change_t *change);
+//import "DPI-C" function int unsigned spect_dpi_get_model_change(dpi_state_change_t *change);
 
 /**
  *  @brief Execute SPECT instruction(s).
@@ -245,6 +245,6 @@ import "DPI-C" function void spect_dpi_set_change_reporting(int enable);
  *  @returns Number of instructions actually executed by this call.
  *  @note Does not check execution time of the instruction.
  */
-import "DPI-C" function int spect_dpi_program_run(int instructions);
+import "DPI-C" function int unsigned spect_dpi_program_run(int unsigned instructions);
 
 `endif // SPECT_ISS_DPI_SVH
