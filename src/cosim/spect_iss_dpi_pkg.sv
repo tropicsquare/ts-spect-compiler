957760fdbb0ebc6444fc27e1cd78eaaec57a443c
Marek Santa <marek.santa@tropicsquare.com>
2022-10-27 12:00:42 +0200

    update DPI pkg to match cpp version

--- src/cosim/spect_iss_dpi_pkg.sv
+++ src/cosim/spect_iss_dpi_pkg.sv
@@ -34,64 +34,60 @@ package spect_iss_dpi_pkg;
     DPI_CHANGE_GPR              = (1 << 0),
     DPI_CHANGE_FLAG             = (1 << 1),
     DPI_CHANGE_MEM              = (1 << 2),
-    DPI_CHANGE_SRR              = (1 << 3),
     DPI_CHANGE_INT              = (1 << 4),
     DPI_CHANGE_RAR              = (1 << 5),
     DPI_CHANGE_RAR_SP           = (1 << 6)
   } dpi_change_kind_t;
 
   typedef struct {
-      dpi_change_kind_t kind;
+    dpi_change_kind_t kind = DPI_CHANGE_GPR;
 
-      // Identifies object on which change occured based on kind
-      //  DPI_CHANGE_GPR:
-      //      0  - GPR0
-      //      ...
-      //      31 - GPR31
-      //
-      //  DPI_CHANGE_FLAG:
-      //      DPI_SPECT_FLAG_ZERO
-      //      DPI_SPECT_FLAG_CARRY
-      //
-      //  DPI_CHANGE_MEM:
-      //      Address in memory on which change occured.
-      //
-      //  DPI_CHANGE_SRR:
-      //      -
-      //
-      //  DPI_CHANGE_INT:
-      //      DPI_SPECT_INT_DONE
-      //      DPI_SPECT_INT_ERR
-      //
-      //  DPI_CHANGE_RAR:
-      //      0 - G_RAR_DEPTH - Index of RAR stack where changed occured
-      //
-      //  DPI_CAHNGE_RAR_SP:
-      //      -
-      int               obj;
+    // Identifies object on which change occured based on kind
+    //  DPI_CHANGE_GPR:
+    //      0  - GPR0
+    //      ...
+    //      31 - GPR31
+    //
+    //  DPI_CHANGE_FLAG:
+    //      DPI_SPECT_FLAG_ZERO
+    //      DPI_SPECT_FLAG_CARRY
+    //
+    //  DPI_CHANGE_MEM:
+    //      Address in memory on which change occured.
+    //
+    //  DPI_CHANGE_INT:
+    //      DPI_SPECT_INT_DONE
+    //      DPI_SPECT_INT_ERR
+    //
+    //  DPI_CHANGE_RAR:
+    //      0 - G_RAR_DEPTH - Index of RAR stack where changed occured
+    //
+    //  DPI_CHANGE_RAR_SP:
+    //      -
+    int unsigned      obj = 0;
 
-      // Old / New value of the object based  on 'kind':
-      //  DPI_CHANGE_GPR:
-      //      0 - Bits 31:0
-      //      ...
-      //      7 - Bits 255:232
-      //
-      //  DPI_CHANGE_FLAG:
-      //      0 - Value of flag
-      //
-      //  DPI_CHANGE_MEM:
-      //      0 - Bits 31:0 of memory location
-      //
-      //  DPI_CHANGE_INT:
-      //      0 - Value of the interrupt
-      //
-      //  DPI_CHANGE_RAR:
-      //      0 - Bits 31:0 of RAR stack location
-      //
-      //  DPI_CAHNGE_RAR_SP:
-      //      Value of RAR stack pointer
-      int               old_val[8];
-      int               new_val[8];
+    // Old / New value of the object based  on 'kind':
+    //  DPI_CHANGE_GPR:
+    //      0 - Bits 31:0
+    //      ...
+    //      7 - Bits 255:232
+    //
+    //  DPI_CHANGE_FLAG:
+    //      0 - Value of flag
+    //
+    //  DPI_CHANGE_MEM:
+    //      0 - Bits 31:0 of memory location
+    //
+    //  DPI_CHANGE_INT:
+    //      0 - Value of the interrupt
+    //
+    //  DPI_CHANGE_RAR:
+    //      0 - Bits 31:0 of RAR stack location
+    //
+    //  DPI_CAHNGE_RAR_SP:
+    //      Value of RAR stack pointer
+    int unsigned      old_val[8] = '{default: 0};
+    int unsigned      new_val[8] = '{default: 0};
   } dpi_state_change_t;
 
 
@@ -335,6 +331,17 @@ package spect_iss_dpi_pkg;
    *  @note Does not check execution time of the instruction.
    */
   import "DPI-C" function int unsigned spect_dpi_program_run(int unsigned instructions);
+
+  /**
+   *  @brief Sets verbosity level of SPECT model
+   *  @param level Verbosity level to set:
+   *                  0 - None        - No debug prints
+   *                  1 - Low         - Only most important debug prints
+   *                  2 - Medium      - Regular debug prints
+   *                  3 - High        - Print everything
+   *  @note Debug prints from model execution to standard output.
+   */
+  import "DPI-C" function void spect_dpi_set_verbosity(int unsigned level);
 
 
 endpackage : spect_iss_dpi_pkg
