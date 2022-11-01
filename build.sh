7b64ce4b2d1c3882b43781ee0570ba5242d15c66
ondrej.ille <ondrej.ille@tropicsquare.com>
2022-11-01 14:46:05 +0100

    compiler: Do not always force rebuild of compiler.

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
 
-rm -rf build
-mkdir build
+if [ "$1" == "--clean" ]; then
+    echo "Deleting previous SPECT compiler build..."
+    rm -rf build
+fi
+mkdir -p build
+
 cd build
 cmake ..
 make
+cd ..
--- test/dpi/dpi_simple_test.cpp
+++ test/dpi/dpi_simple_test.cpp
@@ -13,7 +13,7 @@
 
 
 
-int main(int argc, char** argv)
+int main()
 {
     uint32_t rv;
 
